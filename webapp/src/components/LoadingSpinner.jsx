45754d1f95ab4699ecad87900f87fd0da7f7db69
JenniKate Wallace <jennikate@gmail.com>
2023-05-16 11:48:49 +0100

    New tutorial base

---
+++ webapp/src/components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="lds-ring">
+      Loading
+      <div></div><div></div><div></div><div></div></div>
+  )
+}
+
+export default LoadingSpinner;
